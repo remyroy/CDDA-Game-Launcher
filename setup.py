df7499e49b2d5b1801e89eb7351f44b00a721519
Steve Doe <stevedoe@users.noreply.github.com>
2015-12-21 15:23:56 -0500

    Initial project files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cddagl',
+      version='1.0',
+      description=(
+        'A Cataclysm: Dark Days Ahead launcher with additional features'),
+      author='Rémy Roy',
+      author_email='remyroy@remyroy.com',
+      url='https://github.com/remyroy/CDDA-Game-Launcher',
+      packages=['cddagl'],
+     )
