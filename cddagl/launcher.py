84929cc0d0e4f05ead5f8351960925fa8639ba43
Rémy Roy <remyroy@remyroy.com>
2016-01-08 18:37:49 -0500

    Adding logging and unhandled exception window

--- cddagl/launcher.py
+++ cddagl/launcher.py
@@ -1,5 +1,11 @@
 import sys
 import os
+import traceback
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+from io import StringIO
 
 if getattr(sys, 'frozen', False):
     # we are running in a bundle
@@ -10,10 +16,56 @@ else:
     sys.path.append(basedir)
 
 from cddagl.config import init_config
-from cddagl.ui import start_ui
+from cddagl.ui import start_ui, ui_exception
+
+from cddagl.__version__ import version
+
+MAX_LOG_SIZE = 1024 * 1024
+MAX_LOG_FILES = 5
+
+def init_logging():
+    logger = logging.getLogger('cddagl')
+    logger.setLevel(logging.INFO)
+
+    local_app_data = os.environ['LOCALAPPDATA']
+    if not os.path.isdir(local_app_data):
+        local_app_data = sys.path
+
+    logging_dir = os.path.join(local_app_data, 'CDDA Game Launcher')
+    if not os.path.isdir(logging_dir):
+        os.makedirs(logging_dir)
+
+    logging_file = os.path.join(logging_dir, 'app.log')
+
+    handler = RotatingFileHandler(logging_file, maxBytes=MAX_LOG_SIZE,
+        backupCount=MAX_LOG_FILES, encoding='utf8')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    logger.info('Launcher started: {version}'.format(version=version))
+
+def handle_exception(extype, value, tb):
+    logger = logging.getLogger('cddagl')
+
+    tb_io = StringIO()
+    traceback.print_tb(tb, file=tb_io)
+
+    logger.critical('Global error: Launcher version: {version}, Type: '
+        '{extype}, Value: {value}, Traceback: {traceback}'.format(
+            version=version, extype=str(extype), value=str(value),
+            traceback=tb_io.getvalue()))
+
+    ui_exception(extype, value, tb)
+
+def init_exception_catcher():
+    sys.excepthook = handle_exception
 
 if __name__ == '__main__':
+    init_logging()
+    init_exception_catcher()
 
     init_config(basedir)
-
     start_ui()
--- cddagl/ui.py
+++ cddagl/ui.py
@@ -7,6 +7,8 @@ import random
 import shutil
 import zipfile
 import json
+import traceback
+import html
 
 try:
     from os import scandir
@@ -16,12 +18,12 @@ except ImportError:
 from datetime import datetime, timedelta
 import arrow
 
-from io import BytesIO
+from io import BytesIO, StringIO
 from collections import deque
 
 import html5lib
 from lxml import etree
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlencode
 
 from distutils.version import LooseVersion
 
@@ -39,6 +41,8 @@ from cddagl.config import (
     new_build)
 
 from .__version__ import version
+
+main_app = None
 
 READ_BUFFER_SIZE = 16 * 1024
 
@@ -60,6 +64,7 @@ BASE_URLS = {
 SAVES_WARNING_SIZE = 150 * 1024 * 1024
 
 RELEASES_URL = 'https://github.com/remyroy/CDDA-Game-Launcher/releases'
+NEW_ISSUE_URL = 'https://github.com/remyroy/CDDA-Game-Launcher/issues/new'
 
 WORLD_FILES = set(('worldoptions.json', 'worldoptions.txt', 'master.gsav'))
 
@@ -2070,6 +2075,7 @@ class AboutDialog(QDialog):
     def __init__(self, parent=0, f=0):
         super(AboutDialog, self).__init__(parent, f)
 
+        raise OSError('Testing')
         layout = QGridLayout()
 
         text_content = QTextBrowser()
@@ -2680,8 +2686,83 @@ class ProgressCopyTree(QTimer):
             self.aborted.emit()
 
 
+class ExceptionWindow(QWidget):
+    def __init__(self, extype, value, tb):
+        super(ExceptionWindow, self).__init__()
+
+        layout = QGridLayout()
+
+        information_label = QLabel()
+        information_label.setText('The CDDA Game Launcher just crashed. An '
+            'unhandled exception was raised. Here are the details.')
+        layout.addWidget(information_label, 0, 0)
+        self.information_label = information_label
+
+        tb_io = StringIO()
+        traceback.print_tb(tb, file=tb_io)
+        traceback_content = html.escape(tb_io.getvalue()).replace('\n', '<br>')
+
+        text_content = QTextBrowser()
+        text_content.setReadOnly(True)
+        text_content.setOpenExternalLinks(True)
+        text_content.setHtml('''
+<p>CDDA Game Launcher version: {version}</p>
+<p>Type: {extype}</p>
+<p>Value: {value}</p>
+<p>Traceback:</p>
+<code>{traceback}</code>
+'''.format(version=html.escape(version), extype=html.escape(str(extype)),
+    value=html.escape(str(value)),
+    traceback=traceback_content))
+
+        layout.addWidget(text_content, 1, 0)
+        self.text_content = text_content
+
+        report_url = NEW_ISSUE_URL + '?' + urlencode({
+            'title': 'Unhandled exception: [Enter a title]',
+            'body': '''Description: [Enter what you did and what happened]
+Version: {version}
+Type: `{extype}`
+Value: {value}
+Traceback:
+```
+{traceback}
+```
+'''.format(version=version, extype=str(extype), value=str(value),
+    traceback=tb_io.getvalue())
+        })
+
+        report_label = QLabel()
+        report_label.setOpenExternalLinks(True)
+        report_label.setText('Please help up make a better launcher '
+            '<a href="{url}">by reporting this issue on Github</a>.'.format(
+                url=html.escape(report_url)))
+        layout.addWidget(report_label, 2, 0)
+        self.report_label = report_label
+
+        exit_button = QPushButton()
+        exit_button.setText('Exit')
+        exit_button.clicked.connect(self.close)
+        layout.addWidget(exit_button, 3, 0, Qt.AlignRight)
+        self.exit_button = exit_button
+
+        self.setLayout(layout)
+        self.setWindowTitle('Something went wrong')
+        self.setMinimumSize(350, 0)
+
 def start_ui():
-    app = QApplication(sys.argv)
-    mainWin = MainWindow('CDDA Game Launcher')
-    mainWin.show()
-    sys.exit(app.exec_())
+    global main_app
+
+    main_app = QApplication(sys.argv)
+    main_win = MainWindow('CDDA Game Launcher')
+    main_win.show()
+    main_app.main_win = main_win
+    sys.exit(main_app.exec_())
+
+def ui_exception(extype, value, tb):
+    global main_app
+
+    main_app.closeAllWindows()
+    ex_win = ExceptionWindow(extype, value, tb)
+    ex_win.show()
+    main_app.ex_win = ex_win
