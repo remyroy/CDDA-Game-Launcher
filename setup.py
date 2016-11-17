910906dc62d9846ffefcaed7f5fcf39faf5f0368
Rémy Roy <remyroy@remyroy.com>
2016-11-17 15:08:43 -0500

    Set version to 1.3.7

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ class ExtractUpdateMessages(Command):
 
 
 setup(name='cddagl',
-      version='1.3.6',
+      version='1.3.7',
       description=(
           'A Cataclysm: Dark Days Ahead launcher with additional features'),
       author='Rémy Roy',
