2ab4ff0f10aabe8c6cc71e11da825bb924182ad7
Rémy Roy <remyroy@remyroy.com>
2018-05-10 18:15:20 -0400

    Update version to 1.3.11

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ class ExtractUpdateMessages(Command):
 
 
 setup(name='cddagl',
-      version='1.3.10',
+      version='1.3.11',
       description=(
           'A Cataclysm: Dark Days Ahead launcher with additional features'),
       author='Rémy Roy',
