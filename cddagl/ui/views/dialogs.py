e54e6eb1ee29039cac16cba525edd36dfbd10a1a
KurzedMetal <kurzedmetal@gmail.com>
2019-07-11 17:56:47 -0300

    Avoid translation of the MIT license

--- cddagl/ui/views/dialogs.py
+++ cddagl/ui/views/dialogs.py
@@ -203,22 +203,23 @@ class AboutDialog(QDialog):
                '</ul>')
         m += _('<p>Thanks to <a href="http://mattahan.deviantart.com/">Paul Davey aka Mattahan</a>'
                ' for the permission to use his artwork for the launcher icon.</p>')
-        m += _('<p>Copyright (c) 2015-2019 Rémy Roy</p>')
-        m += _('<p>Permission is hereby granted, free of charge, to any person obtaining a copy'
-               ' of this software and associated documentation files (the "Software"), to deal'
-               ' in the Software without restriction, including without limitation the rights'
-               ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
-               ' copies of the Software, and to permit persons to whom the Software is'
-               ' furnished to do so, subject to the following conditions:</p>'
-               '<p>The above copyright notice and this permission notice shall be included in'
-               ' all copies or substantial portions of the Software.</p>'
-               '<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
-               ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
-               ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
-               ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
-               ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
-               ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
-               ' SOFTWARE.</p>')
+        m += _('<p>This software is distributed under the MIT License</p>')
+        m += '<p>Copyright (c) 2015-2019 Rémy Roy</p>'
+        m += ('<p>Permission is hereby granted, free of charge, to any person obtaining a copy'
+              ' of this software and associated documentation files (the "Software"), to deal'
+              ' in the Software without restriction, including without limitation the rights'
+              ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
+              ' copies of the Software, and to permit persons to whom the Software is'
+              ' furnished to do so, subject to the following conditions:</p>'
+              '<p>The above copyright notice and this permission notice shall be included in'
+              ' all copies or substantial portions of the Software.</p>'
+              '<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+              ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+              ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+              ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+              ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+              ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
+              ' SOFTWARE.</p>')
         self.text_content.setHtml(m)
 
 
